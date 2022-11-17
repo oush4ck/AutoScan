0f00881b57ac2587226844073e8f88f0d1b2765f
anmh4ck2 <116729095+anmh4ck2@users.noreply.github.com>
2022-11-17 16:13:35 +0100

    Actualizador agregado

---
+++ update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+clear
+sudo apt install git -y
+echo -e "\n\n\e[0;34m ACTUALIZANDO...\e[0;37m \n"
+git clone https://github.com/anmh4ck2/AutoScan.git
+if [[ -s AutoScan/autoscan.py ]];then
+cd AutoScan
+cp -r -f * .. > temp
+cd ..
+rm -rf  AutoScan >> temp
+rm temp
+chmod +x autoscan.py
+echo -e "\n\n\e[0;32m ACTUALIZACION FINALIZADA CON ÉXITO !\n\n"
+fi
